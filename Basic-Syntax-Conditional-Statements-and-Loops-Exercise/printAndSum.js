f5a63a14b77bb0f55bd29629daa2a0806964295f
slaveandmaster <slaveandmaster@gmail.com>
2021-05-28 18:02:55 +0300

    edit

--- Basic-Syntax-Conditional-Statements-and-Loops-Exercise/printAndSum.js
+++ Basic-Syntax-Conditional-Statements-and-Loops-Exercise/printAndSum.js
@@ -1,9 +1,12 @@
 function printAndSum(start, end) {
   let sum = 0;
+  let result = '';
   for (let i = start; i <= end; i++) {
-    console.log(i);
+    result += i + ' ';
+    //console.log(i);
     sum += i;
   }
+  console.log(result);
   console.log(`Sum: ${sum}`);
 }
 printAndSum(5, 10);
