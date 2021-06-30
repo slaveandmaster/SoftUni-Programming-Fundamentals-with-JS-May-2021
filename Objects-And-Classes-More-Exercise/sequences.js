a50943c1b81359cbc5b5602740c1c56a700a7580
slaveandmaster <slaveandmaster@gmail.com>
2021-06-30 17:16:01 +0300

    additional task

---
+++ Objects-And-Classes-More-Exercise/sequences.js
@@ -0,0 +1,13 @@
+function sequences(input) {
+   let numbers = input.map((el )=> JSON.parse(el)).map((el) => el.sort((a,b)=> a - b)).map(JSON.stringify);
+   [...new Set(numbers)]
+   .map(JSON.parse)
+   .sort((a,b) => a.length - b.length)
+   .forEach((el) => console.log(`${el.join(', ')}`))
+
+}  
+
+sequences(["[-3, -2, -1, 0, 1, 2, 3, 4]",
+"[10, 1, -17, 0, 2, 13]",
+"[4, -3, 3, -2, 2, -1, 1, 0]"]
+)
